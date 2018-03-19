0db076ba658ef34756ec86b3cb5238fa0ce5a9fa
D7703_06 <d7703_06@D7703-06ui-iMac.local>
2018-03-19 16:01:05 +0900

    add textfield, delegate

--- UITextField/ViewController.swift
+++ UITextField/ViewController.swift
@@ -8,18 +8,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITextFieldDelegate {
 
+    @IBOutlet weak var hello: UILabel!
+    @IBOutlet weak var textField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
+        textField.delegate = self //델리 게이트 연결
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+        
         // Dispose of any resources that can be recreated.
     }
-
+    @IBAction func button(_ sender: Any) {
+        hello.text = "Hello " + textField.text!
+        textField.text = ""
+        textField.resignFirstResponder()
+    }
+    //UItextFieldDelegate method
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        print("I pressed return key")
+        hello.text = "Hello " + textField.text!
+        textField.text = ""
+        textField.resignFirstResponder()
+        return true
+    }
+    // called when 'return' key pressed. return NO to ignore.
 
 }
 
